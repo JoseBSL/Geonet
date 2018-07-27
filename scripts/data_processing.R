f37ed75297e5f3eba4490e239da51b44e3ab5d25
liamkendall <lkendal2@myune.edu.au>
2018-07-27 12:17:01 +1000

    success

--- scripts/data_processing.R
+++ scripts/data_processing.R
@@ -2,8 +2,10 @@
 
 library(kgc)
 library(tidyverse)
+library(reshape)
+library(taxize)
 
-reference=read.csv("data/references.csv",header=T)
+reference=read.csv("data/ref/references.csv",header=T)
 str(reference)
 
 climate_zone=reference[,c("ID","Longitude","Latitude")]
@@ -16,18 +18,14 @@ table(reference$ClimateZ)
 reference[reference$ClimateZ=="Climate Zone info missing",]
 ##GALAPAGOS and MAURITIUS
 
-setwd("~/Dropbox/PhD/Rprojects/Geonet/data")
 
-files = list.files(pattern="*.csv")
+
+
 # First apply read.csv, then rbind
+path <- "data/"
+files = list.files(pattern="*.csv")
 myfiles = lapply(files, function(x) read.csv(x, stringsAsFactors = FALSE, sep=","))
-myfiles[[2]]
 
-myfiles.melt=lapply(myfiles, function(x) melt(x,id.vars=c(1)))
-
-<<<<<<< HEAD
-       tax_name(query = colnames(myfiles[[1]])[2:10], get = "family", db = "ncbi")
-=======
 #Read multiple CSVs
 path <- "data/"
 files <- list.files(path=path, pattern="*.csv")
@@ -37,5 +35,32 @@ for(file in files)
   assign(
     gsub(" ","",substr(file, 1, perpos-1)), 
     read.csv(sep=",",header=T,paste(path,file,sep="")))
-}   
->>>>>>> cd5db2353ad1aefcddd93043350ff1611a027005
+} 
+
+
+
+names(myfiles)=left(files,8)
+names(myfiles)
+
+
+myfiles.melt=lapply(myfiles, function(x) melt(x,id.vars=c(1)))
+myfiles.melt=melt(myfiles,id.vars=c(1))
+myfiles.melt.agg=aggregate(value ~ X + variable+L1,data=myfiles.melt, FUN=sum)
+myfiles.melt.agg$value=ifelse(myfiles.melt.agg$value > 0, 1, 0)
+colnames(myfiles.melt.agg)=c("Plant","Pollinator","Network","Int")
+
+myfiles.melt.agg.z=myfiles.melt.agg[!myfiles.melt.agg$Int==0,]
+
+
+plant_family=tax_name(query=word(myfiles.melt.agg.z$Plant,1),
+                                       get=c("family"),db="ncbi", 
+                                       division_filter = "Plantae",rank_query="genus")[3]
+
+myfiles.melt.agg$Pollinator=gsub("\\."," ",myfiles.melt.agg$Pollinator)
+
+myfiles.melt.agg[,c("Poll.Family","Poll.Order")]=tax_name(query=word(myfiles.melt.agg.z$Pollinator[1:10],1),
+                      get=c("family","order"),db="ncbi", 
+                      division_filter = "Anthropoda",rank_query="genus")[3:4]
+
+
+
