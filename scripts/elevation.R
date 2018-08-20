5553ee7c0160e2a08c2f3fd2d588c1ebfc557598
liamkendall <lkendal2@myune.edu.au>
2018-08-20 15:14:34 +1000

    elevation coding

--- scripts/elevation.R
+++ scripts/elevation.R
@@ -10,3 +10,19 @@ ele2=by(reference, 1:nrow(reference), function(x) GNsrtm3(lat=x$Latitude,lng=x$L
 ele2=do.call("rbind", ele2)
 
 reference$ele=ele2$srtm3
+
+str(geonet)
+
+a=merge(geonet, reference$ele, by = "Network")
+#http://www.gpsvisualizer.com/elevation
+
+reference[reference$ele<0,]
+reference[reference$ID %in% "M_PL_009",c("ele")] = 1009
+reference[reference$ID %in%"M_PL_010",c("ele")] =  261
+reference[reference$ID %in%"M_PL_014",c("ele")] = 250
+reference[reference$ID %in%"M_PL_020",c("ele")] = 250
+reference[reference$ID %in%"M_PL_024",c("ele")] = 92 
+reference[reference$ID %in%"M_PL_026",c("ele")] = 0 #iffy
+reference[reference$ID %in%"M_PL_045",c("ele")] = 0 #iffy
+reference[reference$ID %in%"M_PL_052",c("ele")] = 139 
+reference[reference$ID %in% "M_PL_060",c("ele")] = 0 #iffy
