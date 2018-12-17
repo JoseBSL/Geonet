98d93007ae861a1cab0c80a62c97a32e74bf496f
jstavert <jamie.stavert@gmail.com>
2018-12-17 17:00:20 +1300

    jamie new commit

--- scripts/maps_prop_links_specialisation.R
+++ scripts/maps_prop_links_specialisation.R
@@ -13,35 +13,39 @@ file.remove("ne_110m_admin_0_countries.zip")
 # Load and fortify regular data
 world <- readOGR(".", "ne_110m_admin_0_countries")
 continents.regular <- fortify(world, region="CONTINENT")
+points.zones$zone <- as.factor(points.zones$zone)
 str(continents.regular)
+group.colors <- c(A = "#1b9e77", B = "#d95f02", C ="#66a61e", D = "#e7298a", E = "#7570b3")
 map <- ggplot()
-map <- map + xlab("Longitude") + ylab("Latitude")
-map <- map + geom_map(data=continents.regular,map=continents.regular, aes(map_id=id), colour="black", fill="white", size=0.4) + 
+map <- map + xlab(NULL) + ylab(NULL)
+map <- map + geom_map(data=continents.regular,map=continents.regular, aes(map_id=id), colour="white", fill="white", size=0.001) + 
              expand_limits(x=continents.regular$long, y=continents.regular$lat) + 
              coord_equal()
 map <- map + geom_raster(data=points.zones, 
-                         aes(y=y, x=x, fill=zone2), 
-                         alpha=0.4) 
+                         aes(y=y, x=x, fill=zone), 
+                         alpha=0.7)
+map <- map + scale_fill_manual(values = group.colors)
 map <- map + geom_point(data=g.sub,
-                        aes(x=Longitude, y=Latitude, colour = PolOrder),alpha=0.5,size=0.05/g.sub$prop_links)
+                        aes(x=Longitude, y=Latitude), pch=1, size=0.6+(g.sub$prop_links*8))
+map <- map + ylim(-55, 83.70833)
 map <- map + facet_wrap(~PolOrder,ncol = 3)
-map <- map + theme(axis.line.x = element_line(size=0, colour = "black"),
-                   axis.line.y = element_line(size=0, colour = "black"),
-                   panel.grid.major = element_line(colour = "#d3d3d3"),
-                   panel.grid.minor = element_blank(), panel.background = element_blank()) +
+map <- map + theme(axis.line.x = element_blank(),
+                   axis.line.y = element_blank(),
+                   panel.grid.major = element_line(size=.4, colour = "#d3d3d3"),
+                   panel.grid.minor = element_blank(),
+                   panel.background = element_blank()) +
   theme(axis.text.x=element_text(angle= 360, hjust = 0.5, vjust = 0.5, size =10),
         axis.title.x=element_text(size=16, vjust = 1),
         axis.text.y=element_text(angle= 360, hjust = 0.5, vjust = 0.5, size =10),
         axis.title.y=element_text(size=16, vjust = 1),
         axis.text=element_text(colour = "black"))+
-  theme(axis.ticks.length = unit(2, "mm"))+
+  theme(axis.ticks.length = unit(2, "mm"),
+        axis.ticks = element_line(colour = 'black', size = 0.4))+
   theme(strip.background = element_rect(colour="NA", fill=NA),
         strip.text = element_text(size=12))
 map <- map + theme(axis.title.y=element_text(margin=margin(0,20,0,0)))
-map <- map + scale_colour_brewer(palette="Dark2")
-map <- map + theme(legend.position="none",panel.border = element_rect(color = "black", fill = NA, size = 1))
-map
-ggsave("graphs/links_map.pdf",plot=map,width=15,height=5,units="in")
+map <- map + theme(legend.position="none",panel.border = element_rect(color = "black", fill = NA, size = 0.4))
+ggsave("graphs/links_map_V3.pdf",plot=map,width=15,height=5,units="in")
 
 
 
